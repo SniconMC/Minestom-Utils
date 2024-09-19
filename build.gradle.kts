7b396eb26ed4b59a161e1808e7d33e87bc84df0e
znopp <niklas.termination@gmail.com>
2024-09-19 16:55:09 +0200

    Remove Test Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("net.minestom:minestom-snapshots:65f75bb059") // Minestom
     implementation("ch.qos.logback:logback-classic:1.5.7") // Logback
     implementation("net.kyori:adventure-text-minimessage:4.17.0") // MiniMessage
